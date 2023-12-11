f2d66c7bdade6d12bbc0476bac052cd3879a1003
codingwithchad <codingwithchad@gmail.com>
2023-12-11 13:11:17 -0800

    first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var TestQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Can't connect to db:", err)
+	}
+
+	TestQueries = New(testDB)
+	os.Exit(m.Run())
+}
